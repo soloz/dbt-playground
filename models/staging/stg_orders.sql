a9c5a5493f383ad6da3882bd8ee1b832de0c4515
Solomon Adebayo <solomoniyi@gmail.com>
2026-02-28 22:08:35 +0000

    develop staging orders from customers, products and orders sources

---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,25 @@
+select 
+--from order table
+o.orderid,
+o.orderdate,
+o.shipdate,
+o.shipmode,
+o.ordersellingprice,
+o.ordercostprice,
+o.ordersellingprice - o.ordercostprice as orderprofit,
+
+--from customer table
+c.customername,
+c.segment,
+c.country,
+
+--from product table
+p.category,
+p.productname,
+p.subcategory
+
+from {{ ref('raw_orders') }} o
+left join {{ ref('raw_customers') }} as c
+on o.customerid = c.customerid
+left join {{ ref('raw_products') }} as p
+on o.productid = p.productid
